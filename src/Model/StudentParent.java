078854cdc5c21b3fa95e349f77cf44b47f6ab456
ashishprasad2310 <ashishprasadranjan@gmail.com>
2016-06-22 21:33:11 +0530

    kjh

---
+++ src/Model/StudentParent.java
@@ -0,0 +1,99 @@
+package Model;
+
+public class StudentParent 
+{
+
+    private Model.Parent parent;
+    private Model.Student student;
+	public Model.Parent getParent() {
+		return parent;
+	}
+	public void setParent(Model.Parent parent) {
+		this.parent = parent;
+	}
+	public Model.Student getStudent() {
+		return student;
+	}
+	public void setStudent(Model.Student student) {
+		this.student = student;
+	}
+	
+	/*
+	private int SRollno;
+	private int SBranchId;
+	private int SParentId;
+	private String SFirstName;
+	private String SLastName;
+	private String SPassword;
+	private String SEmailId;
+	
+	private String PParentName;
+	private String PEmailId;
+	private String PPassword;
+	
+	
+	public int getSRollno() {
+		return SRollno;
+	}
+	public void setSRollno(int sRollno) {
+		SRollno = sRollno;
+	}
+	public int getSBranchId() {
+		return SBranchId;
+	}
+	public void setSBranchId(int sBranchId) {
+		SBranchId = sBranchId;
+	}
+	public int getSParentId() {
+		return SParentId;
+	}
+	public void setSParentId(int sParentId) {
+		SParentId = sParentId;
+	}
+	public String getSFirstName() {
+		return SFirstName;
+	}
+	public void setSFirstName(String sFirstName) {
+		SFirstName = sFirstName;
+	}
+	public String getSLastName() {
+		return SLastName;
+	}
+	public void setSLastName(String sLastName) {
+		SLastName = sLastName;
+	}
+	public String getSPassword() {
+		return SPassword;
+	}
+	public void setSPassword(String sPassword) {
+		SPassword = sPassword;
+	}
+	public String getSEmailId() {
+		return SEmailId;
+	}
+	public void setSEmailId(String sEmailId) {
+		SEmailId = sEmailId;
+	}
+	public String getPParentName() {
+		return PParentName;
+	}
+	public void setPParentName(String pParentName) {
+		PParentName = pParentName;
+	}
+	public String getPEmailId() {
+		return PEmailId;
+	}
+	public void setPEmailId(String pEmailId) {
+		PEmailId = pEmailId;
+	}
+	public String getPPassword() {
+		return PPassword;
+	}
+	public void setPPassword(String pPassword) {
+		PPassword = pPassword;
+	}
+	
+	*/
+	
+	
+}
